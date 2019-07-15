f60a6a3a3d3aea0acfbaa8d10b0cb1cd401ca407
Torin-Perkins <tdmp42@gmail.com>
2019-07-15 13:23:52 -0700

    Optimized Ghost Setup
    Changed Ghost to interface

--- src/game/ClydeGhost.java
+++ src/game/ClydeGhost.java
@@ -1,4 +1,57 @@
 package game;
 
-public class ClydeGhost extends Ghost {
+public class ClydeGhost implements Ghost {
+    private int currentPosX = STARTING_X;
+    private int currentPosY = STARTING_Y;
+    private int speed = NORM_SPEED;
+    private boolean scared = false;
+
+    @Override
+    public void setSpeed(boolean slow) {
+        if(slow){
+            speed = SCARED_SPEED;
+        }
+        else{
+            speed = NORM_SPEED;
+        }
+    }
+
+    @Override
+    public void respawn() {
+        setNormal();
+        currentPosX = STARTING_X;
+        currentPosY = STARTING_Y;
+    }
+
+    @Override
+    public void setScared() {
+        setSpeed(true);
+        scared = true;
+    }
+
+    @Override
+    public void setNormal() {
+        setSpeed(false);
+        scared = false;
+    }
+
+    @Override
+    public void move(int x, int y) {
+
+    }
+
+    @Override
+    public boolean getScared() {
+        return scared;
+    }
+
+    @Override
+    public int getCurrentX() {
+        return currentPosX;
+    }
+
+    @Override
+    public int getCurrentY() {
+        return currentPosY;
+    }
 }
