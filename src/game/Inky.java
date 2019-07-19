9a87e7a855a88502979596afa47cf930b5f3b83c
LZhangy <louiszhangy@gmail.com>
2019-07-19 10:08:59 -0700

    documentation

--- src/game/Inky.java
+++ src/game/Inky.java
@@ -23,6 +23,7 @@ public class Inky {
     private int scaredMoves = 0;
     private int maxScaredMoves = 50;
 
+    //Initialize Inky brain
     public Inky() {
         brain = new NeuralNetwork(INPUTS, HIDDEN_ONE, HIDDEN_TWO, OUTPUTS);
         respawn();
@@ -33,6 +34,7 @@ public class Inky {
         respawn();
     }
 
+    //Respawns Inky
     public void respawn() {
         x = 14;
         y = 11;
@@ -40,6 +42,11 @@ public class Inky {
         scared = false;
     }
 
+    /**
+     * processes inputs through Inky's brain into outputs array
+     * @param input the input data
+     * @return the direction with the highest output, which Inky takes
+     */
     private int think(double[] input){
         double[] outputs = brain.calculate(input);
         //double up, down, left, right;
@@ -54,7 +61,14 @@ public class Inky {
         // outputDecision(highestDir);
         return highestDir;
     }
-    
+
+    /**
+     * Lets Inky check immediate adjacent tiles + Pacman's position
+     * @param map the game map
+     * @param px pacman's x coordinate
+     * @param py pacman's y coordinate
+     * @return
+     */
     private double[] see(Tile[][] map, int px, int py) {
         
         double[] inputs = new double[INPUTS];
@@ -75,6 +89,12 @@ public class Inky {
         return inputs;
     }
 
+    /**
+     * Inky's move function
+     * @param map the game map
+     * @param px pacman's x coordinate
+     * @param py pacman's y coordinate
+     */
     public void move(Tile[][] map, int px, int py) {
         if (!alive) respawn();
         dir = think(see(map, px, py));
@@ -82,7 +102,7 @@ public class Inky {
         int prevX = x;
         int prevY = y;
 
-        // Check states
+        // Check scared state
         if (scared) {
             scaredMoves++;
             // Moves every other turn when scared
@@ -126,6 +146,12 @@ public class Inky {
         else if (prevY - y == -2) y--;
     }
 
+    /**
+     * Checks for walls around Inky
+     * @param dir Inky's direction
+     * @param map game map
+     * @return if there is a wall in the way
+     */
     public boolean wallInWay(int dir, Tile[][] map) {
         if (dir == 0) {
             final int testX = x;
@@ -155,13 +181,14 @@ public class Inky {
         return brain;
     }
 
+    //Calculates distance from Pacman
     public double distanceFromPac(int px, int py) {
         return Math.sqrt(Math.pow(px-x, 2) + Math.pow(py-y, 2));
     }
 
-    // Angle to pacman
+    // Calculates angle to pacman
     public double closestDirToPac(int px, int py) {
-        return (Math.sin(y/distanceFromPac(px, py))*distanceFromPac(px, py));
+        return (Math.asin(y/distanceFromPac(px, py))*distanceFromPac(px, py));
     }
 
     /*public double lookUp(Tile[][] map) {
@@ -204,9 +231,8 @@ public class Inky {
         else return 0;
     }
 
-
+    // Print direction
     private void outputDecision(int highestDir) {
-        // Print direction
         if (highestDir == 0) System.out.println("Up output");
         if (highestDir == 1) System.out.println("Left output");
         if (highestDir == 2) System.out.println("Right output");
