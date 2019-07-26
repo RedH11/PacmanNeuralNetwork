761db34f1eff8b491183275c08c96e0553517df3
RedH11 <44552711+RedH11@users.noreply.github.com>
2019-07-26 10:11:52 -0700

    Removing Inky

--- src/game/FitnessComparator.java
+++ src/game/FitnessComparator.java
@@ -2,25 +2,6 @@ package game;
 
 import java.util.Comparator;
 
-class InkyFitnessComparator implements Comparator<PacmanGame> {
-
-    @Override
-    public int compare(PacmanGame x, PacmanGame y) {
-        /* compare logic goes here
-           return a negative number when o1 < o2
-                  a positive number when o1 > o2
-                  0 when o1 == o2
-        */
-
-        double xAvg = (x.getBestInky().fitness + x.getBestInky().fitness2 + x.getBestInky().fitness3) / 3;
-        double yAvg = (y.getBestInky().fitness + y.getBestInky().fitness2 + y.getBestInky().fitness3) / 3;
-
-        if (xAvg < yAvg) return -1;
-        else if (xAvg > yAvg) return 1;
-        return 0;
-    }
-}
-
 class PacmanFitnessComparator implements Comparator<PacmanGame> {
     @Override
     public int compare(PacmanGame x, PacmanGame y) {
@@ -30,8 +11,8 @@ class PacmanFitnessComparator implements Comparator<PacmanGame> {
                   0 when o1 == o2
         */
 
-        double xAvg = (x.pacman.fitness + x.pacman.fitness2 + x.pacman.fitness3) / 3;
-        double yAvg = (y.pacman.fitness + y.pacman.fitness2 + y.pacman.fitness3) / 3;
+        double xAvg = x.pacman.fitness;
+        double yAvg = y.pacman.fitness;
 
         if (xAvg < yAvg) return -1;
         else if (xAvg > yAvg) return 1;
